a074507d366781e4daa195f6bc6c159e0d4297b4
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2024-09-23 09:47:51 -0400

    week 3 class p 2

--- Week3/chapter_4/inclass2/startbootstrap-clean-blog-gh-pages/index.js
+++ Week3/chapter_4/inclass2/startbootstrap-clean-blog-gh-pages/index.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = new express();
+
 app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
 
 app.listen(4000, () => {
   console.log('App listening on port 4000');
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+  // res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+  // res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+  res.render('contact');
 });
 
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+  // res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+  res.render('post');
 });
