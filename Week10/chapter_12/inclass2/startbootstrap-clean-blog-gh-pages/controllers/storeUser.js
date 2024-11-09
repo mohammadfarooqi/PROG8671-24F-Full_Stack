4a7bc5da14ac73f472fe6e9d2972688cb9c85461
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2024-11-09 13:53:39 -0500

    week 10

---
+++ Week10/chapter_12/inclass2/startbootstrap-clean-blog-gh-pages/controllers/storeUser.js
@@ -0,0 +1,22 @@
+const User = require('../models/User.js');
+const path = require('path');
+
+module.exports = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    console.log('user created', user);
+  } catch (error) {
+    console.log('user error', error);
+
+    const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+
+    console.log('validationErrors', validationErrors);
+
+    req.flash('validationErrors',validationErrors);
+    req.flash('data',req.body);
+    // req.session.validationErrors = validationErrors;
+
+    return res.redirect('/auth/register');
+  }
+  res.redirect('/');
+};
